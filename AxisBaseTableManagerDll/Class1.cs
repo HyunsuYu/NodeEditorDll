ac9e1893709b6ddec2eb6e2621dd600356819e9c
HyunsuYu <63137431+HyunsuYu@users.noreply.github.com>
2020-07-24 23:11:00 +0900

    Update

--- AxisBaseTableManagerDll/Class1.cs
+++ AxisBaseTableManagerDll/Class1.cs
@@ -1,7 +1,70 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace AxisBaseTableManager
 {
+    public class AxisBaseTableManager
+    {
+        internal struct Node
+        {
+            
 
+
+
+            internal Node()
+            {
+
+            }
+        }
+
+
+
+
+        private Node[,,] mnodeTable;
+        private AxisBaseTablePalette maxisBaseTablePalette;
+
+
+
+        public AxisBaseTableManager(in AxisBaseTablePalette axisBaseTablePalette)
+        {
+            maxisBaseTablePalette = axisBaseTablePalette;
+            
+        }
+    }
+
+    public class AxisBaseTablePalette
+    {
+        internal struct Node
+        {
+
+        }
+
+
+
+        private int[] mordinaryNum;
+        private List<Node> mnodeTable;
+
+
+
+        public AxisBaseTablePalette()
+        {
+            Random random = new Random();
+
+            mordinaryNum = new int[6];
+            for(int i = 0; i < 6; i++)
+            {
+                mordinaryNum[i] = random.Next(0, 256);
+            }
+
+
+
+            random = null;
+        }
+
+        public int[] OrdinaryNumber
+        {
+            get => mordinaryNum;
+        }
+    }
 }
--- DirectNodeTableManagerTable/DirectNodeTableToolManager.cs
+++ DirectNodeTableManagerTable/DirectNodeTableToolManager.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace DirectNodeTableManagerTool
 {
-    public class DIrectNodeTableToolManager
+    public class DIrectNodeTableManager
     {
 
     }
